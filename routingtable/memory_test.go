2a5a22e95ab0db2b5851069ad7d03a1a165061e6
Robert Kluin <robert.kluin@ezoxsystems.com>
2014-12-20 18:06:15 -0700

    Add MemoryRoutingTable tests

---
+++ routingtable/memory_test.go
@@ -0,0 +1,39 @@
+package routingtable
+
+import (
+	"github.com/robertkluin/message-flow/router"
+	"testing"
+)
+
+type TableTestCase struct {
+	ClientID router.ClientID
+	Result   string
+	Err      *router.RoutingTableError
+}
+
+func TestMemoryGetClientMessageServer(t *testing.T) {
+	table := NewMemoryRoutingTable()
+
+	tests := []TableTestCase{
+		TableTestCase{"NEWCLIENT", "", router.NewRoutingTableError(router.UnknownClient, "")},
+	}
+
+	// Load routing data
+
+	for _, test := range tests {
+		result, err := table.GetClientMessageServer(test.ClientID)
+		if result != test.Result {
+			t.Errorf("FAIL: Results didn't match.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
+				test, result, err)
+		} else if err != nil && test.Err == nil {
+			t.Errorf("FAIL: Got an unexpected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
+				test, result, err)
+		} else if err == nil && test.Err != nil {
+			t.Errorf("FAIL: Didn't get an expected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
+				test, result, err)
+		} else if err != nil && test.Err != nil && err.(router.RoutingTableError).Code != test.Err.Code {
+			t.Errorf("FAIL: Got the wrong error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
+				test, result, err)
+		}
+	}
+}
