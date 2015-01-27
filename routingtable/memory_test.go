6aad0ce0d8f3a5c32c9d632667e41cbe8134362b
Robert Kluin <robert.kluin@ezoxsystems.com>
2015-01-27 14:25:49 -0700

    Refacotr memory tests to reduce duplication

--- routingtable/memory_test.go
+++ routingtable/memory_test.go
@@ -5,36 +5,16 @@ import (
 	"testing"
 )
 
-func TestMemoryGetClientMessageServer(t *testing.T) {
-	table := NewMemoryRoutingTable()
+type TestCase struct {
+	Args   []interface{}
+	Result router.ServerID
+	Err    *router.RoutingTableError
+}
 
-	// Client with no mapped services.
-	table.SetClientMessageServer("client.2", "server.1")
-
-	// Client with a mapped service, but no message server.
-	table.SetClientServiceServer("client.3", "service.1", "server.1")
-
-	type TestCase struct {
-		ClientID router.ClientID
-		Result   router.ServerID
-		Err      *router.RoutingTableError
-	}
-
-	tests := []TestCase{
-		// client.1 does not exist, there are no mappings for it.
-		TestCase{"client.1", "", router.NewRoutingTableError(router.UnknownClient, "")},
-
-		// client.2 messages are mapped to server.1.
-		TestCase{"client.2", "server.1", nil},
-
-		// client.3 has service mappings, but no message server defined.
-		TestCase{"client.3", "", router.NewRoutingTableError(router.MappingNotFoundError, "")},
-	}
-
-	// Load routing data
-
+func evalTests(t *testing.T, tests []TestCase, runner func(test TestCase) (router.ServerID, error)) {
 	for _, test := range tests {
-		result, err := table.GetClientMessageServer(test.ClientID)
+		//result, err := table.GetClientMessageServer(test.ClientID)
+		result, err := runner(test)
 		if result != test.Result {
 			t.Errorf("FAIL: Results didn't match.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
 				test, result, err)
@@ -49,6 +29,36 @@ func TestMemoryGetClientMessageServer(t *testing.T) {
 				test, result, err)
 		}
 	}
+}
+
+func TestMemoryGetClientMessageServer(t *testing.T) {
+	table := NewMemoryRoutingTable()
+
+	// Client with no mapped services.
+	table.SetClientMessageServer("client.2", "server.1")
+
+	// Client with a mapped service, but no message server.
+	table.SetClientServiceServer("client.3", "service.1", "server.1")
+
+	mkArgs := func(clientID router.ClientID) []interface{} {
+		return []interface{}{clientID}
+	}
+
+	tests := []TestCase{
+		// client.1 does not exist, there are no mappings for it.
+		TestCase{mkArgs("client.1"), "", router.NewRoutingTableError(router.UnknownClient, "")},
+
+		// client.2 messages are mapped to server.1.
+		TestCase{mkArgs("client.2"), "server.1", nil},
+
+		// client.3 has service mappings, but no message server defined.
+		TestCase{mkArgs("client.3"), "", router.NewRoutingTableError(router.MappingNotFoundError, "")},
+	}
+
+	evalTests(t, tests, func(test TestCase) (router.ServerID, error) {
+		arg, _ := test.Args[0].(router.ClientID)
+		return table.GetClientMessageServer(arg)
+	})
 }
 
 func TestMemoryGetClientServiceServer(t *testing.T) {
@@ -62,45 +72,29 @@ func TestMemoryGetClientServiceServer(t *testing.T) {
 	table.SetClientServiceServer("client.3", "service.2", "server.1")
 	table.SetClientMessageServer("client.3", "server.2")
 
-	type TestCase struct {
-		ClientID  router.ClientID
-		ServiceID router.ServiceID
-		Result    router.ServerID
-		Err       *router.RoutingTableError
+	mkArgs := func(clientID router.ClientID, serviceID router.ServiceID) []interface{} {
+		return []interface{}{clientID, serviceID}
 	}
 
 	tests := []TestCase{
 		// client.1 does not exist, there are no mappings for it.
-		TestCase{"client.1", "service.1", "", router.NewRoutingTableError(router.UnknownClient, "")},
+		TestCase{mkArgs("client.1", "service.1"), "", router.NewRoutingTableError(router.UnknownClient, "")},
 
 		// client.2 exists, but there are no service mappings for it.
-		TestCase{"client.2", "service.1", "", router.NewRoutingTableError(router.MappingNotFoundError, "")},
+		TestCase{mkArgs("client.2", "service.1"), "", router.NewRoutingTableError(router.MappingNotFoundError, "")},
 
 		// client.3 exists, but there is no service mappings for service.1.
-		TestCase{"client.3", "service.1", "", router.NewRoutingTableError(router.MappingNotFoundError, "")},
+		TestCase{mkArgs("client.3", "service.1"), "", router.NewRoutingTableError(router.MappingNotFoundError, "")},
 
 		// client.3 exists and there is a mapping to service.2 to server.1.
-		TestCase{"client.3", "service.2", "server.1", nil},
+		TestCase{mkArgs("client.3", "service.2"), "server.1", nil},
 	}
 
-	// Load routing data
-
-	for _, test := range tests {
-		result, err := table.GetClientServiceServer(test.ClientID, test.ServiceID)
-		if result != test.Result {
-			t.Errorf("FAIL: Results didn't match.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err == nil {
-			t.Errorf("FAIL: Got an unexpected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err == nil && test.Err != nil {
-			t.Errorf("FAIL: Didn't get an expected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err != nil && err.(*router.RoutingTableError).Code != test.Err.Code {
-			t.Errorf("FAIL: Got the wrong error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		}
-	}
+	evalTests(t, tests, func(test TestCase) (router.ServerID, error) {
+		clientID, _ := test.Args[0].(router.ClientID)
+		serviceID, _ := test.Args[1].(router.ServiceID)
+		return table.GetClientServiceServer(clientID, serviceID)
+	})
 }
 
 func TestMemoryGetServiceServer(t *testing.T) {
@@ -112,39 +106,25 @@ func TestMemoryGetServiceServer(t *testing.T) {
 	// Service with an empty catch-all server.
 	table.SetServiceServer("service.3", "")
 
-	type TestCase struct {
-		ServiceID router.ServiceID
-		Result    router.ServerID
-		Err       *router.RoutingTableError
+	mkArgs := func(serviceID router.ServiceID) []interface{} {
+		return []interface{}{serviceID}
 	}
 
 	tests := []TestCase{
 		// service.1 does not exist, there is no mapping.
-		TestCase{"service.1", "", router.NewRoutingTableError(router.UnknownService, "")},
+		TestCase{mkArgs("service.1"), "", router.NewRoutingTableError(router.UnknownService, "")},
 
 		// service.2 is mapped to server.1.
-		TestCase{"service.2", "server.1", nil},
+		TestCase{mkArgs("service.2"), "server.1", nil},
 
 		// service.3 has a mappings, but no catch-all server defined.
-		TestCase{"service.3", "", router.NewRoutingTableError(router.ServerNotFoundError, "")},
+		TestCase{mkArgs("service.3"), "", router.NewRoutingTableError(router.ServerNotFoundError, "")},
 	}
 
-	for _, test := range tests {
-		result, err := table.GetServiceServer(test.ServiceID)
-		if result != test.Result {
-			t.Errorf("FAIL: Results didn't match.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err == nil {
-			t.Errorf("FAIL: Got an unexpected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err == nil && test.Err != nil {
-			t.Errorf("FAIL: Didn't get an expected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err != nil && err.(*router.RoutingTableError).Code != test.Err.Code {
-			t.Errorf("FAIL: Got the wrong error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		}
-	}
+	evalTests(t, tests, func(test TestCase) (router.ServerID, error) {
+		serviceID, _ := test.Args[0].(router.ServiceID)
+		return table.GetServiceServer(serviceID)
+	})
 }
 
 func TestMemoryGetServiceRegistrar(t *testing.T) {
@@ -163,45 +143,31 @@ func TestMemoryGetServiceRegistrar(t *testing.T) {
 	// Service with an empty registrar.
 	table.SetServiceRegistrar("service.5", "")
 
-	type TestCase struct {
-		ServiceID router.ServiceID
-		Result    router.ServerID
-		Err       *router.RoutingTableError
+	mkArgs := func(serviceID router.ServiceID) []interface{} {
+		return []interface{}{serviceID}
 	}
 
 	tests := []TestCase{
 		// service.1 does not exist, there is no mapping.
-		TestCase{"service.1", "", router.NewRoutingTableError(router.UnknownService, "")},
+		TestCase{mkArgs("service.1"), "", router.NewRoutingTableError(router.UnknownService, "")},
 
 		// service.2 has a server, but no registrar.
-		TestCase{"service.2", "", router.NewRoutingTableError(router.ServerNotFoundError, "")},
+		TestCase{mkArgs("service.2"), "", router.NewRoutingTableError(router.ServerNotFoundError, "")},
 
 		// service.3 has a registrar.
-		TestCase{"service.3", "registrar.1", nil},
+		TestCase{mkArgs("service.3"), "registrar.1", nil},
 
 		// service.4 has a server and a registrar.
-		TestCase{"service.4", "registrar.2", nil},
+		TestCase{mkArgs("service.4"), "registrar.2", nil},
 
 		// service.5 has an empty registrar.
-		TestCase{"service.5", "", router.NewRoutingTableError(router.ServerNotFoundError, "")},
+		TestCase{mkArgs("service.5"), "", router.NewRoutingTableError(router.ServerNotFoundError, "")},
 	}
 
-	for _, test := range tests {
-		result, err := table.GetServiceRegistrar(test.ServiceID)
-		if result != test.Result {
-			t.Errorf("FAIL: Results didn't match.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err == nil {
-			t.Errorf("FAIL: Got an unexpected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err == nil && test.Err != nil {
-			t.Errorf("FAIL: Didn't get an expected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err != nil && err.(*router.RoutingTableError).Code != test.Err.Code {
-			t.Errorf("FAIL: Got the wrong error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		}
-	}
+	evalTests(t, tests, func(test TestCase) (router.ServerID, error) {
+		serviceID, _ := test.Args[0].(router.ServiceID)
+		return table.GetServiceRegistrar(serviceID)
+	})
 }
 
 func TestMemoryGetServiceRandomServer(t *testing.T) {
@@ -245,55 +211,41 @@ func TestMemoryGetServiceRandomServer(t *testing.T) {
 	table.AddServerToServicePool("service.9", "pool.1")
 	table.RemoveServerFromServicePool("service.9", "pool.1")
 
-	type TestCase struct {
-		ServiceID router.ServiceID
-		Result    router.ServerID
-		Err       *router.RoutingTableError
+	mkArgs := func(serviceID router.ServiceID) []interface{} {
+		return []interface{}{serviceID}
 	}
 
 	tests := []TestCase{
 		// service.1 does not exist, there is no mapping.
-		TestCase{"service.1", "", router.NewRoutingTableError(router.UnknownService, "")},
+		TestCase{mkArgs("service.1"), "", router.NewRoutingTableError(router.UnknownService, "")},
 
 		// service.2 has a server, but no server pool.
-		TestCase{"service.2", "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
+		TestCase{mkArgs("service.2"), "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
 
 		// service.3 has a registrar, but no server pool.
-		TestCase{"service.3", "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
+		TestCase{mkArgs("service.3"), "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
 
 		// service.4 has a server and registrar, but no server pool.
-		TestCase{"service.4", "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
+		TestCase{mkArgs("service.4"), "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
 
 		// service.5 has a server, registrar, and single server in the pool.
-		TestCase{"service.5", "pool.1", nil},
+		TestCase{mkArgs("service.5"), "pool.1", nil},
 
 		// service.6 has only a single server in the pool.
-		TestCase{"service.6", "pool.1", nil},
+		TestCase{mkArgs("service.6"), "pool.1", nil},
 
 		// service.7 has an emptied server pool.
-		TestCase{"service.7", "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
+		TestCase{mkArgs("service.7"), "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
 
 		// service.8 has servers added and removed, but one left in the pool.
-		TestCase{"service.8", "pool.2", nil},
+		TestCase{mkArgs("service.8"), "pool.2", nil},
 
 		// service.9 should have an emptied server pool.
-		TestCase{"service.9", "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
+		TestCase{mkArgs("service.9"), "", router.NewRoutingTableError(router.ServerPoolEmptyError, "")},
 	}
 
-	for _, test := range tests {
-		result, err := table.GetServiceRandomServer(test.ServiceID)
-		if result != test.Result {
-			t.Errorf("FAIL: Results didn't match.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err == nil {
-			t.Errorf("FAIL: Got an unexpected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err == nil && test.Err != nil {
-			t.Errorf("FAIL: Didn't get an expected error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		} else if err != nil && test.Err != nil && err.(*router.RoutingTableError).Code != test.Err.Code {
-			t.Errorf("FAIL: Got the wrong error.\n\tTest Case: %+v\n\tActual: {result: \"%v\", err: %+v}",
-				test, result, err)
-		}
-	}
+	evalTests(t, tests, func(test TestCase) (router.ServerID, error) {
+		serviceID, _ := test.Args[0].(router.ServiceID)
+		return table.GetServiceRandomServer(serviceID)
+	})
 }
